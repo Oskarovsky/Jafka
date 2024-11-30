01afd1fccee0c6b503c3e2c7ab63e056abe9d9e2
Oskar Slyk <oskar.slyk@vodeno.com>
2024-11-30 20:15:15 +0100

    jafka

---
+++ src/main/java/com/slyko/jafka/JafkaApplication.java
@@ -0,0 +1,13 @@
+package com.slyko.jafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slyko/jafka/JafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slyko.jafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
